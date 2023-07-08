899fbcfb9d3230de3cc430f17aae1b505d2c7447
422UR4H <tiagosindra@id.uff.br>
2023-07-08 15:15:24 -0300

    project cleaned and initial config added

---
+++ src/assets/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+export default function HomePage() {
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_API_URL}/`).then((res) => {
+            
+        });
+    }, []);
+
+    return (
+        <>
+
+        </>
+    );
+}

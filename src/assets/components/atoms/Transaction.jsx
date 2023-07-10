c488750af3fb089e64fda60fedc24c34ef72dbe8
422UR4H <tiagosindra@id.uff.br>
2023-07-10 16:54:23 -0300

    amount in Transaction replace '.' to ','

--- src/assets/components/atoms/Transaction.jsx
+++ src/assets/components/atoms/Transaction.jsx
@@ -1,9 +1,12 @@
 export default function Transaction({ type, text, amount, time }) {
+    console.log(typeof amount, amount)
     return (
         <div>
             <span className="time">{time}</span>
             <span className="text" data-test="registry-name">{text}</span>
-            <span className="amount" data-test="registry-amount">{amount}</span>
+            <span className="amount" data-test="registry-amount">
+                {amount.toString().replace('.', ',')}
+            </span>
         </div>
     )
 }
